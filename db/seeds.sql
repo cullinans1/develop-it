4971bcf53da273fc003a2d9a9ddf5cbe77841b1f
Sammy <sammy.cullinan@gmail.com>
2020-08-11 09:42:09 -0700

    schema and seeds setup

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO candidates (first_name, last_name, industry_connected)
+VALUES
+('Ronald', 'McDonald', 1),
+('Virginia', 'Woolf', 1),
+('Piers', 'Gaveston', 0),
+('Charles', 'Chocolate', 1),
+('Katherine', 'Mansfield', 1),
+('Dora', 'Explorer', 0),
+('Edward', 'Cullin', 0),
+('Montague', 'Summers', 1),
+('Octavia', 'Butler', 1),
+('Unica', 'Zurn', 1);
