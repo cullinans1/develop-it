9f333b850828f553d16f65e0e2ace6c0b5ce764f
Sammy <sammy.cullinan@gmail.com>
2020-08-13 12:20:23 -0700

    second table added

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,3 +10,9 @@ VALUES
 ('Montague', 'Summers', 1),
 ('Octavia', 'Butler', 1),
 ('Unica', 'Zurn', 1);
+
+INSERT INTO parties (name, description)
+VALUES 
+('JS Juggernauts', 'These people eat and sleep JavaScript'),
+('Heroes of HTML', 'Want to see a mock up turn into a webpage in seconds? Talk to these people'),
+('Git Gurus', 'Nobody knows Git like these folks do');
